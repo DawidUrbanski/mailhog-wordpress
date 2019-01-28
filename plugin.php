a46947b4023673a64cef89fd149a7dc12d80d4b5
insanelab <insanelab@insanelabs-MacBook-Pro.local>
2019-01-28 04:33:46 +0100

    Initial commit

---
+++ plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Plugin Name: Mailhog
+ * Version: 0.1.0
+ * License: GPL2
+ */
+
+add_action('phpmailer_init', function($phpmailer) {
+	$phpmailer->Host = 'mailhog';
+    $phpmailer->Port = 1025;
+    $phpmailer->IsSMTP();
+});
